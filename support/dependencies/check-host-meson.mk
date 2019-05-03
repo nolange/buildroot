08253b81c4140727094a9832b204c0c5bdf6934f
Norbert Lange <nolange79@gmail.com>
2019-05-03 14:30:54 +0200

    prepare build infrastructure to pick up installed meson tool
    Automatically check for an available meson tool,
    and use it as long as the version is fitting.
    
    Using a system provided meson is not an official supported
    option, hence this is currently disabled.
    use of the system provided meson can be enforced with:
    
       make ... BR2_MESON_VERSION_MIN=0
    
    Signed-off-by: Norbert Lange <nolange79@gmail.com>

---
+++ support/dependencies/check-host-meson.mk
@@ -0,0 +1,15 @@
+# Set this to a valid version like  0.49, depending on the highest minimum
+# version required by any of the packages bundled in Buildroot.
+# An empty version will never match a system executable
+#
+# BR2_MESON_VERSION_MIN = 0.49
+
+BR2_MESON_CANDIDATES ?= meson
+BR2_MESON ?= $(call suitable-host-package,meson,\
+	$(BR2_MESON_VERSION_MIN) $(BR2_MESON_CANDIDATES))
+ifeq ($(BR2_MESON),)
+BR2_MESON = $(HOST_DIR)/bin/meson
+BR2_MESON_HOST_DEPENDENCY = host-meson host-ninja
+else
+BR2_MESON_HOST_DEPENDENCY = host-ninja
+endif
