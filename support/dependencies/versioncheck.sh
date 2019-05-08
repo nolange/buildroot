e743fa090f86b5a5391567c0b0d247b63417beca
Norbert Lange <nolange79@gmail.com>
2019-05-08 12:38:47 +0200

    support/dependencies: use a helper script for common checks
    Signed-off-by: Norbert Lange <nolange79@gmail.com>

---
+++ support/dependencies/versioncheck.sh
@@ -0,0 +1,45 @@
+#!/bin/sh
+
+# prevent shift error
+[ $# -ge 2 ] && [ -n "$1" ] || exit 1
+
+split_version() {
+	local VARPREFIX
+	local NUMBERS
+	local major
+	local minor
+
+	VARPREFIX=$1
+	NUMBERS=$2
+
+	major=${NUMBERS%%\.*}
+	NUMBERS=${NUMBERS#$major}; NUMBERS=${NUMBERS#\.}
+	minor=${NUMBERS%%\.*}
+	NUMBERS=${NUMBERS#$minor}; NUMBERS=${NUMBERS#\.}
+
+	# ensure that missing values are 0
+	eval "${VARPREFIX}_major=\$major; ${VARPREFIX}_minor=\$((minor + 0)); ${VARPREFIX}_bugfix=\$((NUMBERS + 0));"
+}
+
+split_version req "$1"
+
+shift
+
+for candidate; do
+
+	# Try to locate the candidate. Discard it if not located.
+	tool=$(which "${candidate}" 2>/dev/null)
+	[ -n "${tool}" ] || continue
+
+	split_version cur "$(readversion "${tool}")"
+
+	[ -n "${cur_major}" -a "${cur_major}" -ge "${req_major}" ] || continue
+	[ "${cur_major}" -gt "${req_major}" ] || [ "${cur_minor}" -ge "${req_minor}" ] || continue
+	[ "${cur_minor}" -gt "${req_minor}" ] || [ "${cur_bugfix}" -ge "${req_bugfix}" ] || continue
+
+	echo "${tool}"
+	exit
+done
+
+# echo nothing: no suitable tool found
+exit 1
