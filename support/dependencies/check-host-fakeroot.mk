d5e54c643d8855458307b246840b07ae545522fd
Norbert Lange <nolange79@gmail.com>
2020-05-06 15:00:19 +0200

    fix fakeroot to be built

--- support/dependencies/check-host-fakeroot.mk
+++ support/dependencies/check-host-fakeroot.mk
@@ -3,7 +3,7 @@
 # package is bumped or a new one added, and it requires a higher
 # version, our cmake infra will catch it and build its own.
 #
-BR2_FAKEROOT_VERSION_MIN = 1.24
+BR2_FAKEROOT_VERSION_MIN = 1.24.2
 
 BR2_FAKEROOT_CANDIDATES ?= fakeroot
 BR2_FAKEROOT ?= $(call suitable-host-package,fakeroot,\
