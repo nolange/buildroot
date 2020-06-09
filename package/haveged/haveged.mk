38ae8f6cccbd37207259cdddceb75f76beba9f5f
Norbert Lange <nolange79@gmail.com>
2020-06-10 01:13:42 +0200

    package/haveged: add option for minimal service binary
    If just the entropy service is needed, the haveged library is
    only built statically, onlinetest and commandmode are disabled.

--- package/haveged/haveged.mk
+++ package/haveged/haveged.mk
@@ -13,6 +13,14 @@ ifeq ($(BR2_sparc_v8)$(BR2_sparc_leon3),y)
 HAVEGED_CONF_OPTS += --enable-clock_gettime=yes
 endif
 
+# TODO:config opt
+ifeq (n,y)
+HAVEGED_CONF_OPTS += --enable-olt=yes
+else
+HAVEGED_CONF_OPTS += --disable-shared --enable-olt=no
+HAVEGED_CONF_ENV += CFLAGS="-DNO_COMMAND_MODE $(TARGET_CFLAGS)"
+endif
+
 define HAVEGED_INSTALL_INIT_SYSV
 	$(INSTALL) -m 755 -D package/haveged/S21haveged \
 		$(TARGET_DIR)/etc/init.d/S21haveged
